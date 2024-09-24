76e0c7aba4ce0900b607a59af873e1d2c9394204
Kevin Vargas <vargaskevv@gmail.com>
2024-09-24 14:25:21 -0400

    add cron UTC timezone

--- cron.go
+++ cron.go
@@ -17,7 +17,7 @@ import (
 )
 
 func setupPriceCron(db *sqlx.DB) {
-	c := cron.New()
+	c := cron.New(cron.WithLocation(time.UTC))
 	// cronExp := "*/5 * * * *" // each 5 minutes
 	cronExp := "0 10,22 * * *" // each day at 06 and 18 hours
 	_, err := c.AddFunc(cronExp, func() {
